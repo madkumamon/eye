56c40f194090b445e50fd6fdaf284b0aa287cf25
sedzen <sedzen@ukr.net>
2012-01-06 13:35:41 +0200

    Rails new app. (.rvmrc,Gemfile,mysql)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eye::Application.config.secret_token = '09acb5d6a142c0ab285d70706178e6779b07088fac2fe39c71087a7e9c33c993a567dafc3f66904c0cb656ac6bdeea11f5ee43768a7a67bcb6dc427c84469d0f'
